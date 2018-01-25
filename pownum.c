feb95b94b4543c8514bacd4e550c028ffafe51de
santhoshshankar <31506543+santhoshshankar@users.noreply.github.com>
2018-01-25 10:47:16 +0000

    Create pownum.c

---
+++ pownum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int base,exp;
+a=10;
+printf("enter the base:");
+scanf("%d",&b);
+printf("enter the exponent:");
+scanf("%d",&e);
+while (e != 0)
+    {
+        result *=e;
+        b--;
+    }
+
+    printf("Answer = %d", a);
+
+    return 0;
+}
