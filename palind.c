dedd9d6e2a0fb3100eea2409adb69e0dc5c90bac
santhoshshankar <31506543+santhoshshankar@users.noreply.github.com>
2018-02-01 14:16:18 +0530

    Create palind.c

---
+++ palind.c
@@ -0,0 +1,15 @@
+3include<stdio.h>
+main()
+{
+int a,rem=0,b;
+printf("enter the values);
+scanf("%d",&a,&b)
+if(a/2==0)
+{
+printf("it is palindrome");
+}
+else
+{
+printf("it is not palindrome);
+}
+}
