7fb7c167bbba4dd9ba449e5219e347c8b43f81ca
santhoshshankar <31506543+santhoshshankar@users.noreply.github.com>
2018-03-09 10:29:24 +0000

    Create isogram.c

---
+++ isogram.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main () 
+{
+    string str1 = "Machine";
+    cout << is_isogram(str1) << endl;
+         
+    string str2 = "isogram";
+    cout << is_isogram(str2) << endl;
+         
+    string str3 = "GeeksforGeeks";
+    cout << is_isogram(str3) << endl;
+         
+    string str4 = "Alphabet";
+    cout << is_isogram(str4) << endl;
+     
+    return 0;
+}
+ 
